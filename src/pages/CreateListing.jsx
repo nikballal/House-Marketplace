81f2ac27596d29147b673fbdbbc5f220c59195eb
nikballal <nikhil.ballal1994@gmail.com>
2022-05-24 22:22:07 +1000

    coordinates with geocoding API

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
+import { toast } from "react-toastify";
 
 function CreateListing() {
   const [geolocationEnabled, setGeolocationEnabled] = useState(true);
@@ -58,9 +59,54 @@ function CreateListing() {
     };
   }, [isMounted]);
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log(formData);
+
+    setLoading(true);
+
+    if (discountedPrice >= regularPrice) {
+      setLoading(false);
+      toast.error("Discounted price needs to be less than regular price");
+      return;
+    }
+
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error("Max 6 images");
+      return;
+    }
+
+    let geolocation = {};
+    let location;
+
+    if (geolocationEnabled) {
+      //using geoapify for the API url instead of google console due to billing
+      const response = await fetch(
+        `https://api.geoapify.com/v1/geocode/search?text=${address}&apiKey=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      );
+      const data = await response.json();
+
+      console.log(data);
+      geolocation.lat = data.features[0]?.geometry.coordinates[0] ?? 0;
+      geolocation.lng = data.features[0]?.geometry.coordinates[1] ?? 0;
+
+      location =
+        data.features.length === 0
+          ? undefined
+          : data.features[0]?.properties.formatted;
+
+      if (location === undefined || location.includes("undefined")) {
+        setLoading(false);
+        toast.error("Please enter a correct address");
+        return;
+      }
+    } else {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+      location = address;
+      console.log(geolocation, location);
+    }
+    setLoading(false);
   };
 
   const onMutate = (e) => {
